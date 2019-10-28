7fb7c64c9344fbce06b2232fdce575f9393bc0a9
Danakt Saushkin <danakt@protonmail.com>
2019-10-28 19:10:59 +0300

    Ignore dist dir from repo

--- dist/types/svg.d.ts
+++
@@ -1,8 +0,0 @@
-import { QrCode } from './qr';
-import { QrOptions } from './types';
-/**
- * Converts QrCode instance to SVG code
- * @param qrCode QrCode instance
- * @param options Convertation options
- */
-export declare const convertSegmentsToSvgString: (qrCode: QrCode, options: Required<QrOptions>) => string;
